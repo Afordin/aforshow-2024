780ecf0ba19eb9b3f03df28fa696ef482e5653d3
Erick Vargas <erianvc.dev@gmail.com>
2024-07-22 22:46:53 -0500

    feat: hero responsive design

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button";
 
 export const Nav = () => {
   return (
-    <nav className="relative w-full max-w-6xl h-16 mx-auto flex items-center justify-between">
+    <nav className="relative px-2 w-full max-w-6xl h-16 mx-auto flex items-center justify-between">
       <a href="/" className="flex items-center gap-1 cursor-pointer">
         <Image src="/logo.png" alt="logo image" width={28} height={28}></Image>
         <h1 className="text-2xl text-white font-semibold">Aforshow</h1>
