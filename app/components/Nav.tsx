bf6c9463727abef2a3b7046587b68132dd90d9f0
alesdev <aleswebgit@gmail.com>
2024-07-30 14:10:52 +0200

    fix: url to logo on Nav component

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -7,7 +7,7 @@ export const Nav = () => {
   return (
     <nav className="relative px-2 w-full max-w-6xl h-16 mx-auto flex items-center justify-between">
       <a href="/" className="flex items-center gap-1 cursor-pointer">
-        <Image src="/logo.png" alt="logo image" width={28} height={28}></Image>
+        <Image src="/imgs/logo.png" alt="logo image" width={28} height={28}></Image>
         <h1 className="text-2xl text-white font-semibold">Aforshow</h1>
       </a>
       <Button>Inscribirse</Button>
