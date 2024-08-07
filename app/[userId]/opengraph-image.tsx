6f28a95ab204f7984da1ef11920a9a3a8da31722
Jarrison Cano <me@jarrison.dev>
2024-08-07 18:39:42 -0500

    refactor: Update opengraph-image.tsx to use dynamic image URL and adjust image height

--- app/[userId]/opengraph-image.tsx
+++ app/[userId]/opengraph-image.tsx
@@ -4,7 +4,7 @@ import { ImageResponse } from 'next/og'
 
 export const size = {
   width: 1200,
-  height: 630,
+  height: 580,
 }
 export const contentType = 'image/png'
 
@@ -16,7 +16,7 @@ export default async function Image({ params: { userId } }) {
   
   if(!user) return new ImageResponse(
     <div style={{display: 'flex', background: '#000000', width:'100%', height: '100%'}}>
-      <img style={{objectFit:'contain'}} src={`${process.env.NEXT_PUBLIC_BASE_URL}/default-og.png`} alt={`Aforshow`} />
+      <img height='100%' style={{objectFit:'contain'}} src={`${process.env.NEXT_PUBLIC_BASE_URL}/default-og.png`} alt={`Aforshow`} />
     </div>
     , { ...size })
 
