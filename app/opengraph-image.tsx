df2ae69859d894e3343b4d4a532a5f7b740aa5e8
Jarrison Cano <me@jarrison.dev>
2024-08-07 16:44:50 -0500

    add objectFit contain to image:og

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -13,7 +13,7 @@ export default async function Image() {
   
   if(!user) return new ImageResponse(
     <div style={{display: 'flex', background: '#000000', width:'100%', height: '100%'}}>
-      <img src={'https://aforshow-2024-five.vercel.app/default-og.png'} alt={`Aforshow`} />
+      <img style={{objectFit:'contain'}} src={'https://aforshow-2024-five.vercel.app/default-og.png'} alt={`Aforshow`} />
     </div>
     , { ...size })
 
