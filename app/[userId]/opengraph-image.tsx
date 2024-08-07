c4688f7e6dbb1229041514629d7c8d30011c5010
Jarrison Cano <me@jarrison.dev>
2024-08-07 17:33:01 -0500

    refactor: Update opengraph-image.tsx to use dynamic image URL

--- app/[userId]/opengraph-image.tsx
+++ app/[userId]/opengraph-image.tsx
@@ -7,9 +7,9 @@ export const contentType = 'image/png'
 
 
 
-export default async function Image() {
+export default async function Image({ params: { userId } }) {
   const apiClient = createClient()
-  const user = (await apiClient.auth.getUser()).data.user
+  const { data: user, error } = await apiClient.from('profiles').select('*').eq('id', userId).single()
   
   if(!user) return new ImageResponse(
     <div style={{display: 'flex', background: '#000000', width:'100%', height: '100%'}}>
--- app/[userId]/page.tsx
+++ app/[userId]/page.tsx
@@ -1,15 +1,18 @@
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
+'use client'
+import { Ticket } from "@/components/Ticket";
+import { User } from "@/store/useUserStore";
+import { apiClient } from "@/utils/supabase/client";
 
-export default async function Page() {
-  const apiClient = createClient();
-  const user = (await apiClient.auth.getUser()).data.user;
+export default async function Page({params: {userId}}) {
+  const { data, error } = await apiClient.from('profiles').select('*').eq('id', userId).single();
+  const user = data as User
 
   return (
     <div>
       <h1>
         {user ? `Hello ${user.email}` : 'Hello stranger'}
       </h1>
+      <Ticket name={user?.name}  avatar={user?.avatar_url} number={user?.count}  />
     </div>
   )
 }
