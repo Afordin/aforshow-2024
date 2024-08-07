073ff00bba08234777572ab8cdf6aa48f8c604bb
Jarrison Cano <me@jarrison.dev>
2024-08-07 15:22:09 -0500

    feat: Update opengraph-image.tsx to use larger image size for better quality

--- app/[userId]/opengraph-image.tsx
+++ app/[userId]/opengraph-image.tsx
@@ -3,7 +3,6 @@ import { createClient } from '@/utils/supabase/server'
 import { ImageResponse } from 'next/og'
 
 
-export const size = { width: 1200, height: 630 }
 
 export default async function Image({ params: { userId } }) {
   const apiClient = createClient()
@@ -11,15 +10,12 @@ export default async function Image({ params: { userId } }) {
   const user = response.data?.[0] as User
   
   if (!user) {
-    return new ImageResponse(<div>Not found</div>, { ...size });
+    return new ImageResponse(<div>Not found</div>, { width: 1200, height: 630 });
   }
 
   const imageUrl = `https://uuljbqkwvruhxomkmxaj.supabase.co/storage/v1/object/public/aforshow/public/${user.id}.png`
 
-  return new ImageResponse(
-    <img src={imageUrl} alt={`${user.name} - Aforshow`} />,
-    { ...size }
-  );
+  return new ImageResponse(<img src={imageUrl} alt={`${user.name} - Aforshow`} />);
   
 
 }
