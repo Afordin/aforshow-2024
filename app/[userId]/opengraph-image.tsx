2b32d1b0aea2daa8ddcadd480296e5ca0ba8fb0c
Jarrison Cano <me@jarrison.dev>
2024-08-07 18:42:53 -0500

    refactor: Update opengraph-image.tsx to center align the default image

--- app/[userId]/opengraph-image.tsx
+++ app/[userId]/opengraph-image.tsx
@@ -15,7 +15,7 @@ export default async function Image({ params: { userId } }) {
   const { data: user, error } = await apiClient.from('profiles').select('*').eq('id', userId).single()
   
   if(!user) return new ImageResponse(
-    <div style={{display: 'flex', background: '#000000', width:'100%', height: '100%'}}>
+    <div style={{display: 'flex',justifyContent:'center', background: '#000000', width:'100%', height: '100%'}}>
       <img height='100%' style={{objectFit:'contain'}} src={`${process.env.NEXT_PUBLIC_BASE_URL}/default-og.png`} alt={`Aforshow`} />
     </div>
     , { ...size })
